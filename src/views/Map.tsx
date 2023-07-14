f38fd2755957dfb69fae79f511f5ae2b6f71ded2
patriciacostadacruz <patriciacstcz@gmail.com>
2023-07-14 13:16:53 +0200

    Displays default map with Barcelona on center

--- src/views/Map.tsx
+++ src/views/Map.tsx
@@ -1,10 +1,30 @@
 import { ReactElement } from 'react';
+import SearchBar from '../components/SearchBar';
 import SearchHistory from '../components/SearchHistory';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 export default function Map(): ReactElement {
+  const mapStyle = {
+    height: '400px',
+    margin: '50px',
+  };
+
+  // Barcelona location
+  const center = {
+    lat: 41.3851,
+    lng: 2.1734,
+  };
+
   return (
     <>
-      <p>Map here</p>
+      <SearchBar />
+      <LoadScript googleMapsApiKey="YOUR_API_KEY">
+        <GoogleMap
+          mapContainerStyle={mapStyle}
+          zoom={13}
+          center={center}
+        ></GoogleMap>
+      </LoadScript>
       <SearchHistory />
     </>
   );
