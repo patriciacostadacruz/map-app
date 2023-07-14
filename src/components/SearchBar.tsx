62d775a98a6095037c38f8990721fba680bdf92d
patriciacostadacruz <patriciacstcz@gmail.com>
2023-07-14 13:09:02 +0200

    Adds search bar input template and styles it

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,14 @@
 import { ReactElement } from 'react';
 
 export default function SearchBar(): ReactElement {
-  return <div>Search bar</div>;
+  return (
+    <div className="search-bar-container">
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="Address or place"
+      />
+      <button className="search-button">Search</button>
+    </div>
+  );
 }
